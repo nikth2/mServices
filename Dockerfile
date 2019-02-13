963a205ba810d0e2c1f885b1a81d5c7522db9edb
Nikos Theodoropoulos <nikth2@gmail.com>
2019-02-13 21:40:37 +0200

    changes
    added debug flags
    added swagger dependency

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ARG JAR_FILE=./target/mservices-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+ENTRYPOINT ["java","-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/org/nikth/HelloRest.java
+++ src/main/java/org/nikth/HelloRest.java
@@ -1,12 +1,25 @@
 package org.nikth;
 
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.nikth.data.DataRepository;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.client.ApiClient;
+import io.swagger.client.api.ActivitiesApi;
+import io.swagger.client.api.AthletesApi;
+import io.swagger.client.auth.Authentication;
+import io.swagger.client.model.DetailedAthlete;
+import io.swagger.client.model.SummaryActivity;
+
 
 @RestController
 public class HelloRest 
@@ -24,8 +37,29 @@ public class HelloRest
         return "userId:"+id +" name:"+dr.getUserById(id).getName();
     }
 	
-	
-	public void getSegments()
+	@RequestMapping(path="/test",produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Map<String,String> test()
 	{
+		ApiClient client = new ApiClient();
+		client.setAccessToken("4764f9b5a7f0bb3e707f197b57656bb4dbf24c22");
+	    io.swagger.client.api.ActivitiesApi api = new ActivitiesApi(client);
+	    try {
+	    	AthletesApi athApi = new AthletesApi(client);
+	    	Map<String, Authentication> m = client.getAuthentications();
+	    	Authentication auth = m.get("strava_oauth");
+	    	//System.out.println(auth.);
+	    	DetailedAthlete result = athApi.getLoggedInAthlete();
+	    	System.out.println(result);
+			//DetailedActivity da = api.getActivityById(new Long(2136435252),Boolean.TRUE);
+	    	List<SummaryActivity> activities = api.getLoggedInAthleteActivities(1549947860, 1541917515, 10, 1);
+	    	//api.get
+			System.out.println(activities);
+			//da.getSegmentEfforts().stream();
+			return Collections.singletonMap("name",activities.get(0).getName());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Collections.singletonMap("error",e.getMessage());
+		}
 	}
 }
