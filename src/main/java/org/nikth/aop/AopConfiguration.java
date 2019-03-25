f26ff3fcf8fc80374acdaedf64e590a433b153f7
Nikos Theodoropoulos <nikth2@gmail.com>
2019-03-25 22:31:22 +0200

    25032019

---
+++ src/main/java/org/nikth/aop/AopConfiguration.java
@@ -0,0 +1,35 @@
+package org.nikth.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@Aspect
+public class AopConfiguration 
+{
+	@Pointcut(
+			"within(org.nikth..*)"
+			//"execution(public String org.nikth.HelloRest.testProperties(..))"
+			)
+	public void monitor() { }
+
+	@Bean
+	public PerformanceMonitorInterceptor performanceMonitorInterceptor() {
+		return new PerformanceMonitorInterceptor(true);
+	}
+
+	@Bean
+	public Advisor performanceMonitorAdvisor() {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("org.nikth.aop.AopConfiguration.monitor()");
+		return new DefaultPointcutAdvisor(pointcut, performanceMonitorInterceptor());
+	}
+}
